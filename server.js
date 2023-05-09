f6af4be54bed4a1103249aada8cbf1da1926ca65
vitali733 <vitalis.weiss@gmail.com>
2023-05-09 19:44:16 +0200

    adding process.env.PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const friesRoutes = require('./routes/friesRoutes.js')
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 const connectDB = require('./db/connectDB.js')
 
 connectDB()
